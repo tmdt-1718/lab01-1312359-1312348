1b37b1143df5e9cef677c1a55bf976383951f384
Dao Chi Minh <1312359@student.hcmus.edu.vn>
2017-10-07 05:33:01 +0000

    Fix image cover

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+    @str = "hello"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
+  get 'login' => 'pages#login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
